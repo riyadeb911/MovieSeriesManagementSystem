232a283f64b7918d11b79eb10f64bce30c2ec829
Riya Dev <riyadev911@gmail.com>
2026-08-27 20:27:53 +0600

    Initial project with database connection

---
+++ MovieSeriesManagementSystem/DataBase/DbConnection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace MovieSeriesManagementSystem.DataBase
+{
+    public class DbConnection
+    {
+        private string connectionString =
+            @"Server=tcp:localhost,1433;Database=MovieSeriesDB;Integrated Security=True;TrustServerCertificate=True;";
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ MovieSeriesManagementSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using MovieSeriesManagementSystem.DataBase;
+
+class Program
+{
+    static void Main()
+    {
+        DbConnection db = new DbConnection();
+
+        try
+        {
+            using (SqlConnection connection = db.GetConnection())
+            {
+                connection.Open();
+
+                Console.WriteLine("Database Connected Successfully!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Database Connection Failed!");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
